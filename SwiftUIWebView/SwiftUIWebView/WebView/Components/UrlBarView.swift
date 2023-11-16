f123613e1a52790956ff8de23677499abab94df0
Delstun McCray <87548497+Delstroo@users.noreply.github.com>
2023-11-16 12:21:13 -0800

    Add .gitignore file

---
+++ SwiftUIWebView/SwiftUIWebView/WebView/Components/UrlBarView.swift
@@ -0,0 +1,18 @@
+//
+//  UrlBarView.swift
+//  SwiftUIWebView
+//
+//  Created by Delstun McCray on 11/16/23.
+//
+
+import SwiftUI
+
+struct UrlBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UrlBarView()
+}
